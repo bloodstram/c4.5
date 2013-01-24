c4e560f7c793f7dac9aa243a6027d1b38623b7d3
jiangw <wj11hk@gmail.com>
2013-01-24 15:26:38 +0800

    refine code structure and add gain function, gain function is unfinished

---
+++ gain.c
@@ -0,0 +1,15 @@
+#include "c4_5.h"
+
+/* compute the GAIN of class variable when the dataset is divided into several subsets by attribute variable */
+
+//function: compute gain for class variable of bool type
+//variable:
+//  dataset - struct array
+//  n - length of dataset
+
+float gainCompute_Bool(struct PlayGolf* dataset, int n)
+{
+    float l_fGain = 0.0f;
+    
+    return l_fGain;
+} 

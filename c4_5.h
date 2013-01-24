89f8a1b5b6035d58a26b7b0863207dd6f5feda29
jiangw <wj11hk@gmail.com>
2013-01-24 15:27:02 +0800

    refine code structure and add gain function, gain function is unfinished

--- c4_5.h
+++ c4_5.h
@@ -5,6 +5,18 @@
 #include <stdbool.h>
 #include <math.h>
 
+enum Outlook_t {SUNNY, OVERCAST, RAINY};
+
+struct PlayGolf
+{
+    enum Outlook_t m_eOutlook;
+    int m_iTemp;
+    int m_iHum;
+    bool m_bWindy;
+    bool m_bPlay;
+};
+
 float entropyCompute_Bool(const bool* classvar, int n);
+float gainCompute_Bool(struct PlayGolf* dataset, int n);
 
 #endif C4_5
--- test.c
+++ test.c
@@ -1,16 +1,5 @@
 #include "c4_5.h"
 
-enum Outlook_t {SUNNY, OVERCAST, RAINY};
-
-struct PlayGolf
-{
-    enum Outlook_t m_eOutlook;
-    int m_iTemp;
-    int m_iHum;
-    bool m_bWindy;
-    bool m_bPlay;
-};
-
 int main()
 {
     //initial dataset
